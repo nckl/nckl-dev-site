069231b6a9fcfafb18d21bfdd96a10553d87fc88
Chris Nickel <chris@nckl.dev>
2022-08-07 10:03:41 -0600

    Add image loader config for cloudflare build error

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+    images: {
+        loader: 'akamai',
+        path: '',
+      },
+  }
+  
+  module.exports = nextConfig
